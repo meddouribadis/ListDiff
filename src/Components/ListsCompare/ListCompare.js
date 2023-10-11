edeb7fa956b1754cc80d8deef344497c0c0bb6ec
Badis MEDDOURI <bmeddouri@inpi.fr>
2023-10-11 16:26:50 +0200

    listA -> listB

--- src/Components/ListsCompare/ListCompare.js
+++ src/Components/ListsCompare/ListCompare.js
@@ -31,8 +31,39 @@ export default function ListCompare() {
         }), {});
         let n = 0;
 
-        for (let t in i) i[t] > 1 && n++;
+        for (let t in i) {
+            i[t] > 1 && n++;
+            //i[t] > 1 && console.log(t, i[t]);
+        }
         return n;
+    }
+
+    const removeDuplicates = (list, setList) => {
+        if (!list || list.length === 0) {
+            setList("");
+            return;
+        }
+
+        let words = list.split(/\r|\r\n|\n/);
+        let cleanedArray = (words = words.map((function (t) {
+            return t.trim()
+        }))).filter((function (t) {
+            return "" !== t
+        }));
+
+        let i = cleanedArray.reduce((function (t, i) {
+            return t[i] = (t[i] || 0) + 1, t
+        }), {});
+
+        let myListArray = [];
+
+        for (let t in i) {
+            if (!myListArray.includes(t)) {
+                myListArray.push(t);
+            }
+        }
+        let c = myListArray.join("\n");
+        setList(c);
     }
 
     const sortList = (list, setList) => {
@@ -43,8 +74,26 @@ export default function ListCompare() {
         let myList = list.replace(/\r?\n$/, "");
         let myListArray = myList.split(/\r?\n/);
         myListArray.sort();
-        let c = myListArray.join("\n"); // You can use "\n" for consistent line breaks
+        let c = myListArray.join("\n");
         setList(c);
+    }
+
+    const reverseList = (list, setList) => {
+        if (!list || list.length === 0) {
+            setList("");
+            return;
+        }
+        let myList = list.replace(/\r?\n$/, "");
+        let myListArray = myList.split(/\r?\n/);
+        myListArray.reverse();
+        let c = myListArray.join("\n");
+        setList(c);
+    }
+
+    const swapList = () => {
+        let temp = listA;
+        setListA(listB);
+        setListB(temp);
     }
 
     return (
@@ -57,40 +106,47 @@ export default function ListCompare() {
                                 <p>List A</p>
                             </div>
                             <div className="count text-right">
-                                <p>{countLines(listA)} - {countDuplicates(listA)}</p>
+                                <p>
+                                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                        {countLines(listA)}
+                                    </span>
+                                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                        {countDuplicates(listA)}
+                                    </span>
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="code-preview-wrapper">
                         <div id="card-list-example" className="flex p-0 bg-white border-gray-200 bg-gradient-to-r code-preview dark:bg-gray-900 border-x dark:border-gray-600">
-                            <textarea value={listA} onChange={(e) => setListA(e.target.value)} name="list-a" id="list-a" rows={10} className="w-full h-auto code-responsive-wrapper shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
+                            <textarea value={listA} onChange={(e) => setListA(e.target.value)} name="list-a" id="list-a" rows={10} className="w-full p-2.5 h-auto code-responsive-wrapper shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
                             </textarea>
                         </div>
                     </div>
                     <div className="footer w-full p-4 border border-gray-200 bg-gray-100 rounded-b-xl dark:border-gray-600 dark:bg-gray-700">
                         <div className="grid grid-cols-3 justify-between">
                             <div className="col-span-1 text-left">
-                                <button onClick={() => { console.log(listA, listA.length) }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button onClick={() => { swapList() }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     A -{">"} B
                                 </button>
                             </div>
                             <div className="col-span-2 text-right">
-                                <button title="Salaam Labass" className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button title="Test" className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdPlaylistRemove size={24} />
                                 </button>
-                                <button title="Supprimer les espaces et les doublons" className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button title="Supprimer les espaces et les doublons" onClick={() => removeDuplicates(listA, setListA)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdLinkOff size={24} />
                                 </button>
                                 <button title="Trier la liste" onClick={() => sortList(listA, setListA)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdSort size={24} />
                                 </button>
-                                <button className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button title="Inverser la liste" onClick={() => reverseList(listA, setListA)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdSwapVert size={24} />
                                 </button>
-                                <button className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button title="Copier" onClick={() => navigator.clipboard.writeText(listA)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdContentCopy size={24} />
                                 </button>
-                                <button className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <button title="Supprimer" onClick={() => setListA("")} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                     <MdDelete size={24} />
                                 </button>
                             </div>
@@ -99,26 +155,55 @@ export default function ListCompare() {
                 </div>
 
                 <div className="list-b">
-                    <div className="w-full p-4 border border-gray-200 bg-gray-50 rounded-t-xl dark:border-gray-600 dark:bg-gray-700">
+                <div className="w-full p-4 border border-gray-200 bg-gray-50 rounded-t-xl dark:border-gray-600 dark:bg-gray-700">
                         <div className="grid grid-cols-2 justify-between text-lg header">
                             <div className="title text-left">
                                 <p>List B</p>
                             </div>
                             <div className="count text-right">
-                                <p>0 - 0</p>
+                                <p>
+                                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                        {countLines(listB)}
+                                    </span>
+                                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                        {countDuplicates(listB)}
+                                    </span>
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div className="code-preview-wrapper">
                         <div id="card-list-example" className="flex p-0 bg-white border-gray-200 bg-gradient-to-r code-preview dark:bg-gray-900 border-x dark:border-gray-600">
-                            <textarea id="list-a" rows={10} className="w-full h-auto code-responsive-wrapper shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
+                            <textarea value={listB} onChange={(e) => setListB(e.target.value)} name="list-a" id="list-a" rows={10} className="w-full p-2.5 h-auto code-responsive-wrapper shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
                             </textarea>
                         </div>
                     </div>
-                    <div className="w-full p-4 border border-gray-200 bg-gray-100 rounded-b-xl dark:border-gray-600 dark:bg-gray-700">
-                        <div className="grid grid-cols-1 header">
-                            <div className="title">
-                                <p>List B</p>
+                    <div className="footer w-full p-4 border border-gray-200 bg-gray-100 rounded-b-xl dark:border-gray-600 dark:bg-gray-700">
+                        <div className="grid grid-cols-3 justify-between">
+                            <div className="col-span-1 text-left">
+                                <button onClick={() => { swapList() }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    A {"<"}- B
+                                </button>
+                            </div>
+                            <div className="col-span-2 text-right">
+                                <button title="Test" className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdPlaylistRemove size={24} />
+                                </button>
+                                <button title="Supprimer les espaces et les doublons" onClick={() => removeDuplicates(listB, setListB)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdLinkOff size={24} />
+                                </button>
+                                <button title="Trier la liste" onClick={() => sortList(listB, setListB)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdSort size={24} />
+                                </button>
+                                <button title="Inverser la liste" onClick={() => reverseList(listB, setListB)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdSwapVert size={24} />
+                                </button>
+                                <button title="Copier" onClick={() => navigator.clipboard.writeText(listB)} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdContentCopy size={24} />
+                                </button>
+                                <button title="Supprimer" onClick={() => setListB("")} className="bg-blue-500 mr-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    <MdDelete size={24} />
+                                </button>
                             </div>
                         </div>
                     </div>
